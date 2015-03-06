e05530cc5dde97b241e53ffd8fb41a9fbdf111fe
timvanhorn <timvanhorn@ymail.com>
2015-03-05 18:47:33 -0600

    test

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -176,5 +176,29 @@ class User extends CI_Controller
 		$this->layout->view('user/edit_profile', $this->data);
 	}
 	
+	function _get_csrf_nonce()
+	{
+		$this->load->helper('string');
+		$key   = random_string('alnum', 8);
+		$value = random_string('alnum', 20);
+		$this->session->set_flashdata('csrfkey', $key);
+		$this->session->set_flashdata('csrfvalue', $value);
+
+		return array($key => $value);
+	}
+
+	function _valid_csrf_nonce()
+	{	
+		if ($this->input->post($this->session->flashdata('csrfkey')) !== FALSE &&
+			$this->input->post($this->session->flashdata('csrfkey')) == $this->session->flashdata('csrfvalue'))
+		{
+			return TRUE;
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
+	
 }
 ?>
