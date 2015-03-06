5ca7a59e3460bc1dcf28290e19ae73151453f063
kylecarlson <krcz85@mail.missouri.edu>
2015-03-06 03:22:32 -0600

    css

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -21,7 +21,9 @@
 				<div class="col-xs-4">
 					<a class="btn btn-default btn-lg wide-button" href="<?php echo base_url('/market/category/' . $category->category_id) ?>"><?php echo $category->name ?></a>
 					<?php foreach ($subcategories->result() as $subcategory) { ?>
-						<a class="btn btn-default btn-xs wide-button" href="<?php echo base_url('/market/category/' . $subcategory->subcategory_id) ?>"><?php echo $subcategory->name ?></a>
+						<?php if ($subcategory->category_id as $category_id->category_id) { ?>
+							<a class="btn btn-default btn-xs wide-button" href="<?php echo base_url('/market/category/' . $subcategory->subcategory_id) ?>"><?php echo $subcategory->name ?></a>
+						<?php } ?>
 					<?php } ?>
 					<br>
 				</div>
