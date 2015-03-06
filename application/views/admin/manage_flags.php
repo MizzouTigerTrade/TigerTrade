06611f25b05ad21be109064368a0be4e55bceb28
timvanhorn <timvanhorn@ymail.com>
2015-03-06 13:32:25 -0600

    start flag page

---
+++ application/views/admin/manage_flags.php
@@ -0,0 +1,48 @@
+<div class="container padding-top-20">
+	<div class="row">
+		<div class="col-xs-3 col-sm-2 text-center">
+			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
+		</div>
+		<div class="col-xs-9 col-sm-10">
+			<h1>Flag Management</h1>
+		</div>
+	</div>
+	
+      <?php if ($message != "") { ?>
+      <div id="infoMessage">
+		<div class="alert alert-info" role="alert" style="margin-top: 10px;">
+		  <span class="sr-only">Error:</span>
+		  <?php echo $message;?>
+		</div>
+	  </div>
+	  <?php }; ?>
+      
+	
+<table class="table">
+	<tr>
+		<th>Ad</th>
+		<th><?php echo lang('index_fname_th');?></th>
+		<th><?php echo lang('index_lname_th');?></th>
+		<th><?php echo lang('index_email_th');?></th>
+		<th>Dismiss Flag</th>
+		<th>Delete Ad</th>
+		<th><?php echo lang('index_status_th');?></th>
+
+	</tr>
+	<?php /* foreach ($users as $user):?>
+		<tr>
+            <td><?php echo htmlspecialchars($user->first_name,ENT_QUOTES,'UTF-8');?></td>
+            <td><?php echo htmlspecialchars($user->last_name,ENT_QUOTES,'UTF-8');?></td>
+            <td><?php echo htmlspecialchars($user->email,ENT_QUOTES,'UTF-8');?></td>
+			<td>
+				<?php foreach ($user->groups as $group):?>
+					<?php echo anchor("auth/edit_group/".$group->id, htmlspecialchars($group->name,ENT_QUOTES,'UTF-8')) ;?> 
+                <?php endforeach?>
+			</td>
+			<td><?php echo ($user->active) ? anchor("auth/deactivate/".$user->id, lang('index_active_link')) : anchor("auth/activate/". $user->id, lang('index_inactive_link'));?></td>
+			<td><?php echo anchor("auth/edit_user/".$user->id, 'Edit') ;?></td>
+		</tr>
+	<?php endforeach; */ ?>
+</table>
+<p><?php echo anchor('auth/create_user', lang('index_create_user_link'))?> | <?php echo anchor('auth/create_group', lang('index_create_group_link'))?></p>
+</div>
