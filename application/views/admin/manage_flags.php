c18c512dae2a814a871ca8bbc46b0f5a155c6654
timvanhorn <timvanhorn@ymail.com>
2015-03-06 17:19:51 -0600

    test

--- application/views/admin/manage_flags.php
+++ application/views/admin/manage_flags.php
@@ -2,27 +2,6 @@
 <link rel="stylesheet" href="<?php echo base_url('assets/js/jquery-ui/jquery-ui.min.css') ?>">
 <script src="<?php echo base_url('assets/js/jquery-ui/external/jquery/jquery.js') ?>"></script>
 <script src="<?php echo base_url('assets/js/jquery-ui/jquery-ui.min.js') ?>"></script>
-
- <script type="text/javascript">
-               function showDialog()
-			   {
-					$( "#dialog-confirm" ).dialog({
-						resizable: false,
-						height:140,
-						modal: true,
-						buttons: {
-							"Delete all items": function() {
-							$( this ).dialog( "close" );
-							},
-							Cancel: function() {
-							$( this ).dialog( "close" );
-							}
-						}
-					});
-				   
-			   }
-			
- </script>
  
  <div id="dialog-confirm" title="Empty the recycle bin?">
   <p><span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 20px 0;"></span>These items will be permanently deleted and cannot be recovered. Are you sure?</p>
@@ -77,3 +56,24 @@
 </table>
 
 </div>
+
+ <script type="text/javascript">
+               function showDialog()
+			   {
+					$( "#dialog-confirm" ).dialog({
+						resizable: false,
+						height:140,
+						modal: true,
+						buttons: {
+							"Delete all items": function() {
+							$( this ).dialog( "close" );
+							},
+							Cancel: function() {
+							$( this ).dialog( "close" );
+							}
+						}
+					});
+				   
+			   }
+			
+ </script>
