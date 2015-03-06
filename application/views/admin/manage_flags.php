26b8f16df540a42d4cf6f626d7d95052b2b3b7b0
timvanhorn <timvanhorn@ymail.com>
2015-03-06 17:42:47 -0600

    test

--- application/views/admin/manage_flags.php
+++ application/views/admin/manage_flags.php
@@ -1,41 +1,4 @@
 
-<link rel="stylesheet" href="<?php echo base_url('assets/js/jquery-ui/jquery-ui.css') ?>">
-<?php /*<script src="<?php echo base_url('assets/js/jquery-ui/external/jquery/jquery.js') ?>"></script>*/ ?>
-<script src="<?php echo base_url('assets/js/jquery-ui/jquery-ui.js') ?>"></script>
- 
- <script>
- 
- 
-   $(function() {
-    $( "#dialog-confirm" ).dialog({
-      resizable: false,
-      height:140,
-      modal: true,
-      buttons: {
-        "Delete all items": function() {
-          $( this ).dialog( "close" );
-        },
-        Cancel: function() {
-          $( this ).dialog( "close" );
-        }
-      }
-    });
-  });
- 
- 
-	 $(document).ready(function() {
-        $("#dialog_open").button().click(function () {
-             $("#dialog_confirm").dialog('open');
-        });
-        });
- 
-
-  </script>
-
-  <div id="dialog-confirm" style="display:none;" title="Empty the recycle bin?">
-  <p><span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 20px 0;"></span>These items will be permanently deleted and cannot be recovered. Are you sure?</p>
-</div>
- 
 <div class="container padding-top-20">
 	<div class="row">
 		<div class="col-xs-3 col-sm-2 text-center">
@@ -75,9 +38,8 @@
             <td><?php echo $flag->flag_count ;?></td>
             <td><?php echo $flag->first_name . " " . $flag->last_name;?></td>
 			<td><?php echo $flag->email ;?> </td>
-			<td><button id="dialog_open" class="btn btn-default">Dismiss</button></td>
+			<td><button class="btn btn-default">Dismiss</button></td>
 			<td><button class="btn btn-default">Delete</button></td>
-			
 			<td><?php echo ($flag->active) ? anchor("auth/deactivate/".$flag->id, lang('index_active_link')) : anchor("auth/activate/". $flag->id, lang('index_inactive_link'));?></td>
 		</tr>
 	<?php endforeach; ?>
