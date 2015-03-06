857bb7224b70589c72820162a188041c0a66043e
kylecarlson <krcz85@mail.missouri.edu>
2015-03-05 21:18:59 -0600

    css

--- application/views/market/category_home.php
+++ application/views/market/category_home.php
@@ -30,7 +30,6 @@
 					<?php } ?>	
 				</select>
 				<select style="margin-bottom: 5px;" onchange="location = this.options[this.selectedIndex].value;" class="form-control input-sm" id="subCategory" name="subCategory">
-			    	<option value="">Subcategory<option>
 					<?php
 						foreach($subcategories->result() as $sub) {
 							if ($category->category_id == $sub->category_id) { ?>
