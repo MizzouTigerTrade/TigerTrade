ee0a7e0098a87800b1d17fae53fe839ea553cd6f
timvanhorn <timvanhorn@ymail.com>
2015-03-06 17:03:53 -0600

    test

--- application/views/admin/manage_flags.php
+++ application/views/admin/manage_flags.php
@@ -1,5 +1,6 @@
 
 <link rel="stylesheet" href="<?php echo base_url('assets/js/jquery-ui/jquery-ui.min.css') ?>">
+<script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
 <script src="<?php echo base_url('assets/js/jquery-ui/jquery-ui.min.js') ?>"></script>
 
  <script type="text/javascript">
