0ed7897606a743193daf29d725e26544fe0a9801
timvanhorn <timvanhorn@ymail.com>
2015-03-06 18:16:45 -0600

    test

--- application/views/admin/manage_flags.php
+++ application/views/admin/manage_flags.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
-<script src="<?php echo base_url('assets/js/jquery.js') ?>"></script>
-<script src="<?php echo base_url('assets/js/jquery-ui/jquery-ui.js') ?>"></script>
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
 
 <script>
 
