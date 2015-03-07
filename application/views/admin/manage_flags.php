04d1dce4f01482bdd89ba1e2a2edd2857dcc8e62
timvanhorn <timvanhorn@ymail.com>
2015-03-07 16:18:29 -0600

    test

--- application/views/admin/manage_flags.php
+++ application/views/admin/manage_flags.php
@@ -84,6 +84,7 @@ $(function($)
 				<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
 				<div class="modal-dialog modal-sm">
 				<div class="modal-content">
+				Are you sure you want to dismiss this Flag?
 				</div>
 				</div>
 				</div>
