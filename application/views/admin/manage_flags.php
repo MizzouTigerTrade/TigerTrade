ce1715621c7707c87b1f04c7a259015056c32f0d
timvanhorn <timvanhorn@ymail.com>
2015-03-07 16:34:43 -0600

    test

--- application/views/admin/manage_flags.php
+++ application/views/admin/manage_flags.php
@@ -34,7 +34,7 @@ $(function($)
 */ ?>
 
 <script src="<?php echo base_url('assets/js/bootstrap.js') ?>"></script>
-<script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.js') ?>"></script>
 
 <div class="container padding-top-20">
 	<div class="row">
