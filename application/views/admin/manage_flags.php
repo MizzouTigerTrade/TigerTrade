e89a30f6e04d525497e562fb429404310c13b8e6
timvanhorn <timvanhorn@ymail.com>
2015-03-06 18:42:24 -0600

    test

--- application/views/admin/manage_flags.php
+++ application/views/admin/manage_flags.php
@@ -4,39 +4,22 @@
 
 <script>
 
-function fnOpenNormalDialog($) {
-    $("#dialog-confirm").html("Confirm Dialog Box");
-
-    // Define the Dialog and its properties.
-    $("#dialog-confirm").dialog({
-        resizable: false,
-        modal: true,
-        title: "Modal",
-        height: 250,
-        width: 400,
-        buttons: {
-            "Yes": function () {
-                $(this).dialog('close');
-                callback(true);
-            },
-                "No": function () {
-                $(this).dialog('close');
-                callback(false);
-            }
+$(function($) {
+    $( "#dialog-confirm" ).dialog({
+      resizable: false,
+      height:140,
+      modal: true,
+      buttons: {
+        "Delete all items": function() {
+          $( this ).dialog( "close" );
+        },
+        Cancel: function() {
+          $( this ).dialog( "close" );
         }
+      }
     });
-}
+  });
 
-	$('#dismissButton').click(fnOpenNormalDialog);
-	
-	function callback(value) {
-    if (value) {
-        alert("Confirmed");
-    } else {
-        alert("Rejected");
-    }
-}
-	
 </script>
 
 <div class="container padding-top-20">
