c3cd4f76ad661e72431b954bb5c42b5b1064f82e
timvanhorn <timvanhorn@ymail.com>
2015-03-07 16:35:36 -0600

    test

--- application/views/admin/manage_flags.php
+++ application/views/admin/manage_flags.php
@@ -33,8 +33,9 @@ $(function($)
 </script>
 */ ?>
 
-<script src="<?php echo base_url('assets/js/bootstrap.js') ?>"></script>
 <script src="<?php echo base_url('assets/js/jquery.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/bootstrap.js') ?>"></script>
+
 
 <div class="container padding-top-20">
 	<div class="row">
