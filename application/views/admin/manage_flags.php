3974e985f02e7ae2eccb0b4e9c322f7736d6a6ef
timvanhorn <timvanhorn@ymail.com>
2015-03-07 16:50:56 -0600

    test

--- application/views/admin/manage_flags.php
+++ application/views/admin/manage_flags.php
@@ -83,8 +83,8 @@ $(function($)
 			<td>
 				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Dismiss</button>
 
-				<div class="modal fade bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-					<div class="modal-dialog">
+				<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-dialog modal-sm">
 						<div class="modal-content">
 							<div class="modal-header">
 								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
