5aac4f354fbb7aaa831fb065f92df6119b0a3cc0
timvanhorn <timvanhorn@ymail.com>
2015-03-12 17:50:35 -0500

    test

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -8,6 +8,8 @@ class Auth extends CI_Controller {
 		$this->load->database();
 		$this->load->library(array('ion_auth','form_validation'));
 		$this->load->helper(array('url','language'));
+		
+		$this->load->model('ad_model');
 		$data['menu'] = $this->load->view('shared/menu');
 
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
