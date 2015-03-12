175c9a26db73ae8cb4411edc90ba8fc7a8cd900c
Tim Gilman <timothy.gilman@yahoo.com>
2015-03-12 18:47:58 -0500

    test

--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -25,64 +25,84 @@
 	  <?php }; ?>
       
 
-      <?php echo form_open("auth/create_user", array('class' => 'form-horizontal', 'id' => 'ad-form'));?>
-            
-
-<form data-toggle="validator" role="form">
-  <div class="form-group">
-    <label for="inputName" class="control-label">Name</label>
-    <input type="text" class="form-control" id="inputName" placeholder="Cina Saffary" required>
-  </div>
-  <div class="form-group">
-    <label for="inputTwitter" class="control-label">Twitter</label>
-    <div class="input-group">
-      <span class="input-group-addon">@</span>
-      <input type="text" pattern="^([_A-z0-9]){3,}$" maxlength="20" class="form-control" id="inputTwitter" placeholder="1000hz" required>
-    </div>
-    <span class="help-block with-errors">Up to 20 letters, numbers and underscores</span>
-  </div>
-  <div class="form-group">
-    <label for="inputEmail" class="control-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail" placeholder="Email" data-error="Bruh, that email address is invalid" required>
-    <div class="help-block with-errors"></div>
-  </div>
-  <div class="form-group">
-    <label for="inputPassword" class="control-label">Password</label>
-    <div class="form-group col-sm-6">
-      <input type="password" data-minlength="6" class="form-control" id="inputPassword" placeholder="Password" required>
-      <span class="help-block">Minimum of 6 characters</span>
-    </div>
-    <div class="form-group col-sm-6">
-      <input type="password" class="form-control" id="inputPasswordConfirm" data-match="#inputPassword" data-match-error="Whoops, these don't match" placeholder="Confirm" required>
-      <div class="help-block with-errors"></div>
-    </div>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="radio">
-      <label>
-        <input type="radio" name="underwear" required>
-        Boxers
-      </label>
-    </div>
-    <div class="radio">
-      <label>
-        <input type="radio" name="underwear" required>
-        Briefs
-      </label>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="checkbox">
-      <label>
-        <input type="checkbox" id="terms" data-error="Before you wreck yourself" required>
-        Check yourself
-      </label>
-      <div class="help-block with-errors"></div>
-    </div>
-  </div>
-  <div class="form-group">
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </div>
+      <form data-toggle="validator" role="form">
+            <div class="form-group">
+                  <label for="first_name" class="col-sm-4 control-label label-20">First Name</label>
+                  <div class="col-sm-4">
+                        <?php
+	                         $data = array(
+					          'name'        => 'first_name',
+					          'id'          => 'first_name',
+					          'class'       => 'form-control'
+					        );
+	                        echo form_input($data);
+	                    ?>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <label for="last_name" class="col-sm-4 control-label label-20">Last Name</label>
+                  <div class="col-sm-4">
+	                    <?php
+	                         $data = array(
+					          'name'        => 'last_name',
+					          'id'          => 'last_name',
+					          'class'       => 'form-control'
+					        );
+	                        echo form_input($data);
+	                    ?>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <label for="email" class="col-sm-4 control-label label-20">Email</label>
+                  <div class="col-sm-4">
+                        <?php
+	                         $data = array(
+					          'name'        => 'email',
+					          'id'          => 'email',
+					          'class'       => 'form-control'
+					        );
+	                        echo form_input($data);
+	                    ?>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <label for="phone" class="col-sm-4 control-label label-20">Phone</label>
+                  <div class="col-sm-4">
+                        <?php
+	                         $data = array(
+					          'name'        => 'phone',
+					          'id'          => 'phone',
+					          'class'       => 'form-control'
+					        );
+	                        echo form_input($data);
+	                    ?>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <label for="password" class="col-sm-4 control-label label-20">Password</label>
+                  <div class="col-sm-4">
+                        <input type="password" data-minlength="6" class="form-control" id="inputPassword" placeholder="Password" required>
+      					<span class="help-block">Minimum of 6 characters</span>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <label for="password_confirm" class="col-sm-4 control-label label-20">Confirm Password</label>
+                  <div class="col-sm-4">
+                        <?php
+	                         $data = array(
+					          'name'        => 'password_confirm',
+					          'id'          => 'password_confirm',
+					          'class'       => 'form-control'
+					        );
+	                        echo form_password($data);
+	                    ?>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <div class="col-sm-offset-4 col-sm-4">
+                        <button type="submit" class="btn btn-default">Create User</button>
+                  </div>
+            </div>
+      </form>
       
 </div>
