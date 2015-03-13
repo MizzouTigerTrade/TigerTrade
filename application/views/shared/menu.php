94b7d2a6e691860ae6feb678a192f6645dbffca5
timvanhorn <timvanhorn@ymail.com>
2015-03-13 15:16:19 -0500

    test

--- application/views/shared/menu.php
+++ application/views/shared/menu.php
@@ -55,7 +55,7 @@
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $user->first_name; ?> <span class="badge badge-info" style="background-color: red;"><?php if($total_offer_notification>0){echo $total_offer_notification ;} ?></span> <b class="caret"></b></a>
 						<ul class="dropdown-menu">
 							<li><a href="<?php echo base_url('/ad/user_ads') ?>">My Ads</a></li>
-							<li><a href="<?php echo base_url('/offers/sent') ?>">Sent Offers  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <span class="badge badge-info" style="background-color: red;"><?php if($sent_offer_notification>0){echo $sent_offer_notification ;} ?></span></a></li>
+							<li><a href="<?php echo base_url('/offers/sent') ?>">Sent Offers  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <span class="badge badge-info" style="background-color: red;"><?php if($sent_offer_notification>0){echo $sent_offer_notification ;} ?></span></a></li>
 							<li><a href="<?php echo base_url('/offers/received') ?>">Received Offers &nbsp <span class="badge badge-info" style="background-color: red;"><?php if($received_offer_notification>0){echo $received_offer_notification ;} ?></span></a></li>
 							<li><a href="<?php echo base_url('/user/edit_profile/' . $this->ion_auth->get_user_id() ) ?>">Edit Profile</a></li>
 							<li><a href="<?php echo base_url('/user') ?>">User</a></li>
