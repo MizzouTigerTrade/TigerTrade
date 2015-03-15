f4681ab48d4d220d82bcf28249e288209ce9e8c8
timvanhorn <timvanhorn@ymail.com>
2015-03-15 17:39:41 -0500

    test

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -124,7 +124,7 @@ class Admin extends CI_Controller {
 		<title>HTML email</title>
 		</head>
 		<body>
-		<img src='assets/images/ttradeimage.png' alt='Tiger Trade' />
+		<img src='assets/Images/ttradeimage.png' alt='Tiger Trade' />
 		<p>" 
 		. $message_to_user . 
 		"</p></body>
