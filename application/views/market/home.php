7cab7154235d0c291e06147d51417acb9f82032d
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-02 18:58:52 -0500

    add search bar

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -104,6 +104,16 @@
 					</div>
 				</div>
 
+				<div id="custom-search-input">
+                    <div class="input-group col-md-12">
+                        <input type="text" class="  search-query form-control" placeholder="Search" />
+                        <span class="input-group-btn">
+                            <button class="btn btn-danger" type="button">
+                                <span class=" glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
+                    </div>
+                </div>
 				<!-- Display Ads: rows of 1 -->
 				
 				<?php foreach ($ads->result() as $row) { ?>
