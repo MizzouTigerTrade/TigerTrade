3035b314d24127be36696f91936ccae7c8a8a5c2
kylecarlson <krcz85@mail.missouri.edu>
2015-04-02 17:49:40 -0500

    css

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -52,4 +52,34 @@
 	<?php } ?>
 	
 	<!-- Devices == Extra Small (Mobile) -->
+	<div class="row visible-xs">
+		<div class="col-sm-7">
+			<p style="font-size: .9em;"><?php echo $category->name; ?><?php echo $subcategory; ?></p>
+			<h2 style="margin-top: 10px;">Asking Price: <span style="color: green;">$<?php echo $ad->price; ?></span></h2>
+			<a class="btn btn-default" href="<?php echo base_url('/ad/make_offer/' . $ad->ad_id) ?>">Make an Offer</a>
+			<a class="btn btn-warning" href="<?php echo base_url('/ad/flag_ad/' . $ad->ad_id) ?>">Flag Ad</a>
+			<p class="text-justify" style="font-size: 1.1em; margin-top: 10px;">Details: <?php echo $ad->description; ?></p>
+		</div>
+		<div class="col-sm-5">
+				<img class="img-thumbnail" src="http://placehold.it/500x500" alt="ad_image" width="100%">
+		</div>
+	</div>
+	<?php if ($this->ion_auth->logged_in()) { ?>
+		<div class="row" style="margin-top: 20px;">
+			<div class="col-xs-12" style="padding: 0;">
+				<div class="form-group">
+					<label for="buyer_message" class="col-xs-12 control-label label-20">Comment:</label>
+					<div class="col-xs-12">
+						<textarea type="text" class="form-control description-box" name="comment" id="comment" rows="5"></textarea>
+						<div class="col-sm-8">
+							<p class="help-block">Post a comment anonymously. Please be respectful.</p>
+						</div>
+						<div class="col-sm-4">
+							<button class="btn btn-sm btn-default">FUCKING SUBMIT BUTTON</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php } ?>
 </div>
