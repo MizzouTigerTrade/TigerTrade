619645fa4c1f188a4dd2bfcabbd6000cd1f33148
kylecarlson <krcz85@mail.missouri.edu>
2015-04-02 17:44:29 -0500

    css

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -24,9 +24,9 @@
 		<div class="col-sm-7">
 			<p style="font-size: .9em;"><?php echo $category->name; ?><?php echo $subcategory; ?></p>
 			<h2 style="margin-top: 10px;">Asking Price: <span style="color: green;">$<?php echo $ad->price; ?></span></h2>
-			<p class="text-justify">Details: <?php echo $ad->description; ?></p>
 			<a class="btn btn-default" href="<?php echo base_url('/ad/make_offer/' . $ad->ad_id) ?>">Make an Offer</a>
 			<a class="btn btn-warning" href="<?php echo base_url('/ad/flag_ad/' . $ad->ad_id) ?>">Flag Ad</a>
+			<p class="text-justify" style="font-size: 1.1em;">Details: <?php echo $ad->description; ?></p>
 		</div>
 		<div class="col-sm-5">
 				<img class="img-thumbnail" src="http://placehold.it/500x500" alt="ad_image" width="100%">
