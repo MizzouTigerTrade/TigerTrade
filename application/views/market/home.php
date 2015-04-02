bb6a0c64a5e3d59065f689f4bfd1fb014ea1adab
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-02 18:11:58 -0500

    php

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -4,7 +4,7 @@
 			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
 		</div>
 		<div class="col-xs-9 col-sm-10">
-			<h1>Market: </h1>
+			<h1>Market: <?php echo $category_id; ?></h1>
 		</div>
 	</div>
 	
