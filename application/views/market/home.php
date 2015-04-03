f6709b04c73a72b4b39cf815ea1c82718fe04487
kylecarlson <krcz85@mail.missouri.edu>
2015-04-02 19:09:56 -0500

    test

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -25,10 +25,7 @@
 
 <div class="container padding-top-20">
 	<div class="row">
-		<div class="col-xs-3 col-sm-2 text-center">
-			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
-		</div>
-		<div class="col-xs-9 col-sm-10">
+		<div class="col-xs-12">
 			<h1>Market: <?php echo $market_name; ?></h1>
 		</div>
 	</div>
