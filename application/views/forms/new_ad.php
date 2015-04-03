5361a021ce501bf2837cff8ef048de3794b487ca
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-02 20:03:41 -0500

    html

--- application/views/forms/new_ad.php
+++ application/views/forms/new_ad.php
@@ -100,7 +100,7 @@ $(document).ready(function (){
 		</div>
 
 		<div class="form-group">
-			<label for="description" class="col-sm-2 control-label label-20">Tags</label>
+			<label for="description" class="col-sm-2 control-label label-20">Tags (seperate by comma)</label>
 			<div class="col-sm-10">
 				<textarea type="text" class="form-control description-box" name="tags" id="tags" rows="2"></textarea>
 			</div>
