f2d7329f7bb0839f61f5ca50f83275349ba751d1
kylecarlson <krcz85@mail.missouri.edu>
2015-04-02 19:40:12 -0500

    css

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -128,7 +128,7 @@ $(document).ready(function(){
 				<!-- custom search bar -->
 				<div id="custom-search-input" style="margin-top: 10px">
                     <div class="input-group col-xs-12 col-sm-10 col-sm-offset-1">
-                        <input type="text" class="search-query form-control" id="filter" placeholder="Search" />
+                        <input type="text" class="search-query input-sm form-control" id="filter" placeholder="Search" />
                         <span class="input-group-btn">
                             <button class="btn btn-danger" type="button">
                                 <span class=" glyphicon glyphicon-search"></span>
