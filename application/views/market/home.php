cf653045212da28a73f26d90d6c8adf2716cafaa
kylecarlson <krcz85@mail.missouri.edu>
2015-04-02 19:37:52 -0500

    css

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -49,7 +49,7 @@ $(document).ready(function(){
 
 <div class="container padding-top-20">
 	<div class="row">
-		<div class="col-xs-12">
+		<div class="col-xs-10 col-xs-offset-1">
 			<h1>Market: <?php echo $market_name; ?></h1>
 		</div>
 	</div>
