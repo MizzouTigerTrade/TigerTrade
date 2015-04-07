9a9ba0f4398be0eba7d935c976113bd0dd455f10
timvanhorn <timvanhorn@ymail.com>
2015-04-06 20:45:58 -0500

    test

--- application/models/subcategory_model.php
+++ application/models/subcategory_model.php
@@ -40,8 +40,9 @@ class Subcategory_model extends CI_Model
 	*/
 
 	public function get_subcategories($category_id){
-		$this->db->order_by("name", "asc"); 
+		
         $this->db->select('subcategory_id, name');
+		$this->db->order_by("name", "asc"); 
         $this->db->where('category_id', $category_id);
         $query = $this->db->get('subcategories');
         $subcategories = array();
