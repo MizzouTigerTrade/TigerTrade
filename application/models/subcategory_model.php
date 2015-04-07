0ce27dbfb91101486837ff17b7238e5b32b61b44
timvanhorn <timvanhorn@ymail.com>
2015-04-06 20:35:21 -0500

    test

--- application/models/subcategory_model.php
+++ application/models/subcategory_model.php
@@ -31,18 +31,38 @@ class Subcategory_model extends CI_Model
 		return $this->db->get('subcategories');
 	}
 
+	/*
 	public function get_subcategories($category_id)
 	{
 		$query = $this->db->query("SELECT * FROM subcategories WHERE category_id = '$category_id' ORDER BY name ASC");
 		return $query;
 	}
+	*/
 
+	public function get_subcategories($category_id){
+        $this->db->select('subcategory_id, name');
+        $this->db->where('category_id', $category_id);
+        $query = $this->db->get('subcategories');
+        $subcategories = array();
+
+        if($query->result()){
+            foreach ($query->result() as $sub) {
+                $subcategories[$sub->subcategory_id] = ucfirst($sub->name);
+            }
+            return $subcategories;
+        } else {
+            return FALSE;
+        }
+    } 
+	
 	public function get_subcategory($subcategory_id)
 	{
 		$query = $this->db->query("SELECT * FROM subcategories WHERE subcategory_id = '$subcategory_id'");
 		$query = $query->row();
 		return $query;
 	}
+	
+	
 	
 	public function get_subcategory_by_name($name)
 	{
