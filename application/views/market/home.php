5dc1b7c311ef11c3c1a72293b39f379b811e7370
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-08 17:52:24 -0500

    html

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -148,7 +148,7 @@ $(document).ready(function(){
 						</div>
 						<div class="media-body col-xs-9 col-md-8 search">
 							<h4 class="media-heading"><?php echo $row->title; ?>: $<?php echo $row->price; ?></h4> 
-							<?php foreach($tags->result() as $tag)) { 
+							<?php foreach($tags->result() as $tag) { 
 									if($tag->ad_id == $row->ad_id)
 									{
 										echo $tag->description . ' ';
