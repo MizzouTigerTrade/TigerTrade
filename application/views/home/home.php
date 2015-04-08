44ed6ec3bc7c2066515cbe329ebeb4041ec6ef14
kylecarlson <krcz85@mail.missouri.edu>
2015-04-08 18:22:05 -0500

    carousel

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,15 +1,45 @@
 <div class="jumbotron">
 	<div class="container">
-		<h1>Welcome to TigerTrade!</h1>
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-	
-		<p>If you would like to edit this page you'll find it located at: <code>application/views/home/home.php</code></p>
-	
-		<p>The corresponding controller for this page is found at: <code>application/controllers/home.php</code></p>
-	
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="http://getbootstrap.com/getting-started/">User Guide</a>.</p>
 		
-		<!--<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>-->
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+				<li data-target="#myCarousel" data-slide-to="3"></li>
+			</ol>
+			
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox" style="height: 400px;">
+				<div class="item active">
+					<img src="" alt="Chania">
+				</div>
+					
+				<div class="item">
+					<img src="" alt="Chania">
+				</div>
+					
+				<div class="item">
+					<img src="" alt="Flower">
+				</div>
+					
+				<div class="item">
+					<img src="" alt="Flower">
+				</div>
+			</div>
+			
+			<!-- Left and right controls -->
+			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+
 	</div>
 </div>
 
