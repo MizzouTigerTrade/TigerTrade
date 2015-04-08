d4a5a09efa4cfbf239c18a72b446f78d43a1c002
timvanhorn <timvanhorn@ymail.com>
2015-04-08 18:50:58 -0500

    test query

--- application/models/ad_model.php
+++ application/models/ad_model.php
@@ -121,7 +121,7 @@ class Ad_model extends CI_Model
 	public function get_flagged_ads()
 	{
 		//$query = $this->db->query("SELECT * FROM ads JOIN users ON ads.user_id = users.id WHERE flag_count > 0 ORDER BY flag_count DESC");
-		$query = $this->db->query("SELECT * FROM flags JOIN users ON ads.user_id = users.id GROUP BY ad_id");
+		$query = $this->db->query("SELECT * FROM flags JOIN ads ON flags.ad_id = ads.ad_id JOIN users ON ads.user_id = users.id GROUP BY flags.ad_id");
 		$result = $query->result();
 		return $result;
 	}
