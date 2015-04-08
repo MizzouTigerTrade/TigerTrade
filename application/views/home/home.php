5a53ee327c96549c9a1a383e3ac01fd9a98fb802
kylecarlson <krcz85@mail.missouri.edu>
2015-04-08 18:23:20 -0500

    css

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,46 +1,46 @@
 <div class="jumbotron">
-	<div class="container">
-		
-		<div id="myCarousel" class="carousel slide" data-ride="carousel">
-			<!-- Indicators -->
-			<ol class="carousel-indicators">
-				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-				<li data-target="#myCarousel" data-slide-to="1"></li>
-				<li data-target="#myCarousel" data-slide-to="2"></li>
-				<li data-target="#myCarousel" data-slide-to="3"></li>
-			</ol>
-			
-			<!-- Wrapper for slides -->
-			<div class="carousel-inner" role="listbox" style="height: 400px;">
-				<div class="item active">
-					<img src="" alt="Chania">
-				</div>
-					
-				<div class="item">
-					<img src="" alt="Chania">
-				</div>
-					
-				<div class="item">
-					<img src="" alt="Flower">
-				</div>
-					
-				<div class="item">
-					<img src="" alt="Flower">
-				</div>
-			</div>
-			
-			<!-- Left and right controls -->
-			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
-		</div>
 
+		
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+			<li data-target="#myCarousel" data-slide-to="3"></li>
+		</ol>
+		
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox" style="height: 100%;">
+			<div class="item active">
+				<img src="" alt="Chania">
+			</div>
+				
+			<div class="item">
+				<img src="" alt="Chania">
+			</div>
+				
+			<div class="item">
+				<img src="" alt="Flower">
+			</div>
+				
+			<div class="item">
+				<img src="" alt="Flower">
+			</div>
+		</div>
+		
+		<!-- Left and right controls -->
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
 	</div>
+
+
 </div>
 
 <div class="container">
