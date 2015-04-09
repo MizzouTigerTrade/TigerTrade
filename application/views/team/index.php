7ecddb61ed4883cf39d1b38632a10f4b97b9401a
kylecarlson <krcz85@mail.missouri.edu>
2015-04-08 19:47:23 -0500

    css

--- application/views/team/index.php
+++ application/views/team/index.php
@@ -10,22 +10,22 @@
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2"><h2>Kyle Carlson</h2></div>
 		<div class="col-sm-2 col-sm-offset-2"><img class="img-thumbnail" src="http://placehold.it/500x500" width="100%" height="100%"></div>
-		<div class="col-sm-6 col-sm-offset-2">DESCRIPTION</div>
+		<div class="col-sm-6">DESCRIPTION</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2"><h2>Jason McQuinn</h2></div>
 		<div class="col-sm-2 col-sm-offset-2"><img class="img-thumbnail" src="http://placehold.it/500x500" width="100%" height="100%"></div>
-		<div class="col-sm-6 col-sm-offset-2">DESCRIPTION</div>	
+		<div class="col-sm-6">DESCRIPTION</div>	
 	</div>
 	<div class="row">		
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2"><h2>Tim Gilman</h2></div>
 		<div class="col-sm-2 col-sm-offset-2"><img class="img-thumbnail" src="http://placehold.it/500x500" width="100%" height="100%"></div>
-		<div class="col-sm-6 col-sm-offset-2">DESCRIPTION</div>	
+		<div class="col-sm-6">DESCRIPTION</div>	
 	</div>
 	<div class="row">		
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2"><h2>Tim Van Horn</h2></div>
 		<div class="col-sm-2 col-sm-offset-2"><img class="img-thumbnail" src="http://placehold.it/500x500" width="100%" height="100%"></div>
-		<div class="col-sm-6 col-sm-offset-2">DESCRIPTION</div>	
+		<div class="col-sm-6">DESCRIPTION</div>	
 	</div>
 </div>
 				
