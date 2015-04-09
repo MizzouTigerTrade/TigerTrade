51af7e3486c50e63feba5ffb5ce9635eba11db8a
timvanhorn <timvanhorn@ymail.com>
2015-04-08 19:00:05 -0500

    flag delete

--- application/models/ad_model.php
+++ application/models/ad_model.php
@@ -196,7 +196,8 @@ class Ad_model extends CI_Model
 	{
 		$this->db->set('flag_count', 0);
 		$this->db->where('ad_id', $ad_id);
-		
+	
+		/*
 		if( $this->db->update('ads') != TRUE)
 		{
 			throw new Exception("Cannot Update Flag Count");
@@ -205,6 +206,10 @@ class Ad_model extends CI_Model
 		{
 			return $this->db->affected_rows();
 		}
+		*/
+		
+		$this->db->where('ad_id', $ad_id);
+		$this->db->delete('flags'); 
 	}
 
 	public function get_user_ads($user_id)
