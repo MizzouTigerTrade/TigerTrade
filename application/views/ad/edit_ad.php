c6e0668147bb756ce89a2b0453b87bd169d16aec
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-08 19:42:28 -0500

    js

--- application/views/ad/edit_ad.php
+++ application/views/ad/edit_ad.php
@@ -34,7 +34,7 @@ $(document).ready(function (){
 <?php foreach($tags->result() as $tag) { ?>
 	var tag = "<?php echo $tag->description; ?>";
 	alert(tag);
-	$('#tags').val($('#tags').val() + tag, ', ');
+	$('#tags').val($('#tags').val() + tag + ', ');
 <?php }?>
 });
 
