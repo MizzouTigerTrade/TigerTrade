ccc2e433baf99f1757ff0e8edd459d6779df9e67
kylecarlson <krcz85@mail.missouri.edu>
2015-04-08 19:14:40 -0500

    css

--- application/views/team/index.php
+++ application/views/team/index.php
@@ -1,10 +1,13 @@
 <div class="container padding-top-20">
 	<div class="row">
 		<div class="col-xs-12">
-			<h1>TigerTrade Web Site Terms and Conditions of Use</h1>
+			<h1>TigerTrade Development Team</h1>
 		</div>
 	</div>
 
-	<h2>Meet the development team:</h2>	
+	<h2>Kyle Carlson</h2>	
+	<h2>Jason McQuinn</h2>	
+	<h2>Tim Gilman</h2>	
+	<h2>Tim Van Horn</h2>	
 </div>
 				
