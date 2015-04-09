3ed78b3ef97ef58c5f6089f531acf7c9820e9c4f
kylecarlson <krcz85@mail.missouri.edu>
2015-04-09 18:38:17 -0500

    css

--- application/views/shared/menu.php
+++ application/views/shared/menu.php
@@ -84,9 +84,9 @@
 					<li class="dropdown <?php if (in_array($this->uri->segment(1), array('user', 'offers'))) { ?>active<?php } ?>">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $user->first_name; ?> <span class="badge badge-info" style="background-color: #D43F3A;"><?php if($total_offer_notification>0){echo $total_offer_notification ;} ?></span> <b class="caret"></b></a>
 						<ul class="dropdown-menu">
-							<li><a href="<?php echo base_url('/ad/user_ads') ?>">My Ads <span class="glyphicon glyphicon-pencil pull-right"></span></a></li>
-							<li><a href="<?php echo base_url('/offers/sent') ?>">Sent Offers <span class="badge badge-info" style="background-color: #d9534e;"><?php if($sent_offer_notification>0){echo $sent_offer_notification ;} ?></span><span class="glyphicon glyphicon-pencil pull-right"></span></a></li>
-							<li><a href="<?php echo base_url('/offers/received') ?>">Received Offers <span class="badge badge-info" style="background-color: #D43F3A;"><?php if($received_offer_notification>0){echo $received_offer_notification ;} ?></span><span class="glyphicon glyphicon-pencil pull-right"></span></a></li>
+							<li><a href="<?php echo base_url('/ad/user_ads') ?>">My Ads <span class="glyphicon glyphicon-pencil dropdown-menu-right"></span></a></li>
+							<li><a href="<?php echo base_url('/offers/sent') ?>">Sent Offers <span class="badge badge-info" style="background-color: #d9534e;"><?php if($sent_offer_notification>0){echo $sent_offer_notification ;} ?></span></a></li>
+							<li><a href="<?php echo base_url('/offers/received') ?>">Received Offers <span class="badge badge-info" style="background-color: #D43F3A;"><?php if($received_offer_notification>0){echo $received_offer_notification ;} ?></span></a></li>
 							<li><a href="<?php echo base_url('/user/edit_profile/' . $this->ion_auth->get_user_id() ) ?>">Edit Profile</a></li>
 						</ul>
 					</li>
