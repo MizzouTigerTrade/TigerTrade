6cd36ed3f53967d09aefe68e81b60779a8f83814
kylecarlson <krcz85@mail.missouri.edu>
2015-04-08 19:49:03 -0500

    css

--- application/views/team/index.php
+++ application/views/team/index.php
@@ -1,3 +1,10 @@
+<style>
+.team-member-description {
+	padding-top: 5px;
+	font-size: 2em;
+}
+</style>
+
 <div class="container padding-top-20">
 	<div class="row">
 		<div class="col-xs-12">
@@ -10,22 +17,22 @@
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2"><h2>Kyle Carlson</h2></div>
 		<div class="col-sm-2 col-sm-offset-2"><img class="img-thumbnail" src="http://placehold.it/500x500" width="100%" height="100%"></div>
-		<div class="col-sm-6">DESCRIPTION</div>
+		<div class="col-sm-6 team-member-description">DESCRIPTION</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2"><h2>Jason McQuinn</h2></div>
 		<div class="col-sm-2 col-sm-offset-2"><img class="img-thumbnail" src="http://placehold.it/500x500" width="100%" height="100%"></div>
-		<div class="col-sm-6">DESCRIPTION</div>	
+		<div class="col-sm-6 team-member-description">DESCRIPTION</div>	
 	</div>
 	<div class="row">		
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2"><h2>Tim Gilman</h2></div>
 		<div class="col-sm-2 col-sm-offset-2"><img class="img-thumbnail" src="http://placehold.it/500x500" width="100%" height="100%"></div>
-		<div class="col-sm-6">DESCRIPTION</div>	
+		<div class="col-sm-6 team-member-description">DESCRIPTION</div>	
 	</div>
 	<div class="row">		
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2"><h2>Tim Van Horn</h2></div>
 		<div class="col-sm-2 col-sm-offset-2"><img class="img-thumbnail" src="http://placehold.it/500x500" width="100%" height="100%"></div>
-		<div class="col-sm-6">DESCRIPTION</div>	
+		<div class="col-sm-6 team-member-description">DESCRIPTION</div>	
 	</div>
 </div>
 				
