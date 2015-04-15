6adb31cd985ce2212c483271434294b58c4cd221
jasonmcquinn <jdmnr9@mail.missouri.edu>
2015-04-15 15:35:36 -0500

    testing

--- application/controllers/ad.php
+++ application/controllers/ad.php
@@ -244,7 +244,7 @@ class Ad extends CI_Controller
 	}
 	
 	//adds a comment to the ad
-	function comment($ad_id)
+	function comment_ad($ad_id)
 	{
 		$this->form_validation->set_rules('comment', 'Comment', 'required');
 		/*
