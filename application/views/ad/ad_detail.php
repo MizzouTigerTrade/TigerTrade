97f91a554a5e05c5f1414919b455f59da2960371
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-16 17:49:34 -0500

    php

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -53,7 +53,7 @@
 			}
 		?>
 		<div class="col-sm-5">
-				if(empty($image_link)) { ?> 
+				<?php if(empty($image_link)) { ?> 
 					<img class="img-thumbnail" src="http://placehold.it/500x500" alt="" width="100%" height="100%">
 				<?php } else { ?>
 					<img class="img-thumbnail" src="<?php echo $image_link; ?>" alt="Error loading image" width="100%" height="100%">
