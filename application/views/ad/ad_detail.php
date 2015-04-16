9ea21775ec036cb27dc7a56c42dd762e3f0ade9a
jasonmcquinn <jdmnr9@mail.missouri.edu>
2015-04-16 18:52:27 -0500

    test

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -56,6 +56,7 @@
 			<div class="col-xs-12" style="padding: 0;">
 				<?php echo form_open("ad/comment", array('class' => 'form-horizontal', 'id' => 'comment-form', 'enctype' => 'multipart/form-data'));?>	
 				<?php echo form_hidden('ad_id', $ad->ad_id); ?>
+				<a class="btn btn-xs btn-default" href="<?php echo base_url('/ad/user_comments/' . $ad->ad_id) ?>">Show Comments</a>
 				<label for="comments" class="col-sm-2 control-label label-20">Comment</label>
 				<div class="form-group">			
 					<div class="col-sm-10">
