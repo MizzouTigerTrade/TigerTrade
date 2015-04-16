f1fe78ace461da204a3a5c61a5194f7fff5eebd6
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-16 17:47:53 -0500

    php

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -153,7 +153,7 @@ $(document).ready(function(){
 										}
 									}
 							
-							if(empty($image_link) { ?> 
+							if(empty($image_link)) { ?> 
 								<img class="img-thumbnail" src="http://placehold.it/500x500" alt="" width="100%" height="100%">
 								<?php } else { ?>
 							<img class="img-thumbnail" src="<?php echo $image_link; ?>" alt="http://placehold.it/500x500" width="100%" height="100%">
