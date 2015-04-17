867eb6efd73a03e1785c7daa5d940d2b0bd0de89
timvanhorn <timvanhorn@ymail.com>
2015-04-17 15:56:06 -0500

    fix erro

--- application/views/offers/sent.php
+++ application/views/offers/sent.php
@@ -44,7 +44,7 @@
 
 	<hr>
 	
-	<button type="button" id="pending_button" class="btn btn-default offers-button">Pending</button> <button type="button" id="accepted_button" class="btn btn-default offers-button">Accepted</button> <button type="button" id="declined_button" class="btn btn-default offers-button">Declined</button>
+	<button type="button" id="pending_button" class="btn btn-default btn-primary offers-button">Pending</button> <button type="button" id="accepted_button" class="btn btn-default offers-button">Accepted</button> <button type="button" id="declined_button" class="btn btn-default offers-button">Declined</button>
 	
 	<div id="pending">
 	<h3>Pending</h3>
