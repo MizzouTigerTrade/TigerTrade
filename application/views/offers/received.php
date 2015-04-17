cf8bda57b656edb5560622575544dcdac7b802f6
timvanhorn <timvanhorn@ymail.com>
2015-04-17 16:35:12 -0500

    test styling

--- application/views/offers/received.php
+++ application/views/offers/received.php
@@ -7,27 +7,27 @@
         $('#pending').hide();
 		$('#declined').hide();
 		$('#accepted').show();
-		$('#accepted_button').addClass('btn-info');
-		$('#declined_button').removeClass('btn-info');
-		$('#pending_button').removeClass('btn-info');
+		$('#accepted_button').addClass('btn-warning');
+		$('#declined_button').removeClass('btn-warning');
+		$('#pending_button').removeClass('btn-warning');
         });
 		
 		$('#declined_button').click(function() {
         $('#pending').hide();
 		$('#accepted').hide();
 		$('#declined').show();
-		$('#accepted_button').removeClass('btn-info');
-		$('#declined_button').addClass('btn-info');
-		$('#pending_button').removeClass('btn-info');
+		$('#accepted_button').removeClass('btn-warning');
+		$('#declined_button').addClass('btn-warning');
+		$('#pending_button').removeClass('btn-warning');
 		});
 		
 		$('#pending_button').click(function() {
         $('#accepted').hide();
 		$('#declined').hide();
 		$('#pending').show();
-		$('#accepted_button').removeClass('btn-info');
-		$('#declined_button').removeClass('btn-info');
-		$('#pending_button').addClass('btn-info');
+		$('#accepted_button').removeClass('btn-warning');
+		$('#declined_button').removeClass('btn-warning');
+		$('#pending_button').addClass('btn-warning');
         });
 		
 	});
@@ -43,18 +43,17 @@
 
 	<hr>
 
-	<button type="button" id="pending_button" class="btn btn-default btn-info offers-button">Pending</button> <button type="button" id="accepted_button" class="btn btn-default offers-button">Accepted</button> <button type="button" id="declined_button" class="btn btn-default offers-button">Declined</button>
+	<button type="button" id="pending_button" class="btn btn-default btn-warning offers-button">Pending</button> <button type="button" id="accepted_button" class="btn btn-default offers-button">Accepted</button> <button type="button" id="declined_button" class="btn btn-default offers-button">Declined</button>
 
 	<div id="pending">
-	<h3>Pending</h3>
+	<!--<h3>Pending</h3>-->
 	<div class="table-responsive">
 		<table class="table table-hover">
 			<tr style="background-color: white;">
 				<th>Ad Name</th>
-				<!--<th>Buyer ID</th>
-				<th>Seller ID</th>-->
 				<th>Buyer Message</th>
 				<th>Price</th>
+				<th></th>
 			</tr>
 			<?php foreach ($pending->result() as $row) { ?>
 			<tr style="cursor: hand;" class='clickable-row' data-href='<?php echo base_url('/offers/review_offer/' . $row->offer_id) ?>'>
@@ -72,7 +71,7 @@
 	
 	<div id="accepted" style="display: none;">
 	<div class="table-responsive">
-		<h3>Accepted</h3>
+		<!--<h3>Accepted</h3>-->
 		<table class="table table-hover">
 			<tr>
 				<th>Ad Name</th>
