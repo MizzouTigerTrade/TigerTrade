71a102db9420140692aad9b3da85e4990717f6e3
timvanhorn <timvanhorn@ymail.com>
2015-04-17 16:51:40 -0500

    change styling

--- application/views/offers/sent.php
+++ application/views/offers/sent.php
@@ -8,27 +8,27 @@
         $('#pending').hide();
 		$('#declined').hide();
 		$('#accepted').show();
-		$('#accepted_button').addClass('btn-info');
-		$('#declined_button').removeClass('btn-info');
-		$('#pending_button').removeClass('btn-info');
+		$('#accepted_button').addClass('btn-warning');
+		$('#declined_button').removeClass('btn-warning');
+		$('#pending_button').removeClass('btn-warning');
         });
 		
 		$('#declined_button').click(function() {
         $('#pending').hide();
 		$('#accepted').hide();
 		$('#declined').show();
-		$('#accepted_button').removeClass('btn-info');
-		$('#declined_button').addClass('btn-info');
-		$('#pending_button').removeClass('btn-info');
+		$('#accepted_button').removeClass('btn-warning');
+		$('#declined_button').addClass('btn-warning');
+		$('#pending_button').removeClass('btn-warning');
 		});
 		
 		$('#pending_button').click(function() {
         $('#accepted').hide();
 		$('#declined').hide();
 		$('#pending').show();
-		$('#accepted_button').removeClass('btn-info');
-		$('#declined_button').removeClass('btn-info');
-		$('#pending_button').addClass('btn-info');
+		$('#accepted_button').removeClass('btn-warning');
+		$('#declined_button').removeClass('btn-warning');
+		$('#pending_button').addClass('btn-warning');
         });
 		
 	});
@@ -44,7 +44,7 @@
 
 	<hr>
 	
-	<button type="button" id="pending_button" class="btn btn-default btn-info offers-button">Pending</button> <button type="button" id="accepted_button" class="btn btn-default offers-button">Accepted</button> <button type="button" id="declined_button" class="btn btn-default offers-button">Declined</button>
+	<button type="button" id="pending_button" class="btn btn-default btn-warning offers-button">Pending</button> <button type="button" id="accepted_button" class="btn btn-default offers-button">Accepted</button> <button type="button" id="declined_button" class="btn btn-default offers-button">Declined</button>
 	<br>
 	<br>
 	
