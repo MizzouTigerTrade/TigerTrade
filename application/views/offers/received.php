fdf9edf4b3ce7fd26190376f9d333b43120a7fc6
timvanhorn <timvanhorn@ymail.com>
2015-04-18 13:15:48 -0500

    test

--- application/views/offers/received.php
+++ application/views/offers/received.php
@@ -70,9 +70,49 @@
 								Offer Price: <?php echo "$" . $row->offer_price; ?><br>
 								Asking Price: <?php echo "$" . $row->asking_price; ?><br>
 								Buyer Message: <?php echo $row->buyer_message; ?><br>
+								
+								<?php echo form_open("offers/response/" . $row->offer_id, array('class' => 'form-horizontal', 'id' => 'offer-response-form', 'enctype' => 'multipart/form-data'));?>
+		
+								<div class="form-group">
+									<label for="description" class="col-sm-2 control-label label-20">Reply</label>
+									<div class="col-sm-10">
+										<textarea type="text" name="seller_response" class="form-control description-box" id="seller_response" rows="5"></textarea>
+										<p class="help-block">Write a message for the buyer, including good times to meet or preferred method of contact.</p>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="status" class="col-sm-2 control-label label-20">Offer Decision</label>
+									<div class="col-sm-10">
+										<div class="radio">
+											<label>
+												<input type="radio" id='accept-offer' name="status" id="status1" value="Accepted" required="true">
+												Accept Offer<p class="text-danger" id='offer-warning' style='display: none; padding-left: 10px'> You and the buyer will exchange contact info.</p>
+											</label>
+										</div>
+										<div class="radio">
+											<label>
+												<input type="radio" name="status" id="status2" value="Declined">
+												Decline Offer
+											</label>
+										</div>
+									</div>
+								</div>
+								
+								<div class="form-group">
+									<div class="col-sm-offset-2 col-sm-10">
+										<div class="checkbox">
+											<label>
+												<input type="checkbox" required="true"> <a href="<?php echo base_url('/content/terms') ?>">I Agree to the Terms & Conditions</a>
+											</label>
+										</div>
+									</div>
+								</div>
+								
 							</div>
 							<div class="modal-footer">
-								<a class="btn btn-xs btn-primary" href="">Send</a>
+								<input class="btn btn-xs btn-primary" type="submit" value="Yes">
+								</form>
 								<button type="button" class="btn btn-xs btn-default" data-dismiss="modal">Cancel</button>
 							</div>
 						</div>
