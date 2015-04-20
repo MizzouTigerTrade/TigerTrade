ba8deece39206a10acd20dfb54a22f47ff040f8e
timvanhorn <timvanhorn@ymail.com>
2015-04-20 16:43:36 -0500

    fix sql default value error

--- application/sql/tigertrade.sql
+++ application/sql/tigertrade.sql
@@ -102,8 +102,8 @@ CREATE TABLE kylecarlson_tigertrade.offers (
 	buyer_message BLOB,
 	seller_response BLOB,
 	status VARCHAR(10) DEFAULT "Pending",
-	seen_by_buyer BOOLEAN DEFAULT true,
-	seen_by_seller BOOLEAN DEFAULT false
+	seen_by_buyer BOOLEAN DEFAULT 1,
+	seen_by_seller BOOLEAN DEFAULT 0
 );
 
 DROP TABLE IF EXISTS `groups`;
