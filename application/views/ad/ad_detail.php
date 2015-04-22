c4db25b02011e320578ea3c0f4b02d49e794d4ca
jasonmcquinn <jdmnr9@mail.missouri.edu>
2015-04-22 18:55:35 -0500

    test

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -55,7 +55,7 @@
 	<?php if ($this->ion_auth->logged_in()) { ?>
 		<div class="row" style="margin-top: 20px;">
 			<label for="comments" class="col-sm-10">View Comments:</label>
-			<?php foreach($ad->comments as $row) { ?>
+			<?php foreach($comments as $row) { ?>
 				<div class="col-xs-12">
 					<p style="font-size: .9em;"><?php echo $row; ?></p>
 				</div>
