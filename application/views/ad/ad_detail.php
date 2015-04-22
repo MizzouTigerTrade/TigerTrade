8534ec3529a787482c2cc05042fdb35cffa75ce4
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-22 17:46:25 -0500

    php

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -15,7 +15,59 @@
 		</div>
 	  </div>
 	<?php }; ?>
+
+	<?php echo $images->count();?>
+<!--
+	<div id="myCarousel" class="carousel slide" data-ride="carousel" style="background-color: rgba(0, 0, 0, 0.17);">
+
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+	</ol>
 	
+
+	<div class="carousel-inner text-center" role="listbox" style="min-height: 300px;">
+		<div class="item active">
+			<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1" style="padding-bottom: 45px;">
+				<h1 style="font-size: 3.5em; margin-top: 30px;">Welcome to the TigerTrade!</h1>
+				<p style="font-size: 1.5em;">Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem.</p>
+			</div>
+		</div>
+			
+		<div class="item">
+			<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1" style="padding-bottom: 45px;">
+				<h1 style="font-size: 3.5em; margin-top: 30px;">Your source for all things Mizzou!</h1>
+				<p style="font-size: 1.5em;">Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem.</p>
+			</div>
+		</div>
+			
+		<div class="item">
+			<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1" style="padding-bottom: 45px;">
+				<h1 style="font-size: 3.5em; margin-top: 30px;">Buy!</h1>
+				<p style="font-size: 1.5em;">Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem.</p>
+			</div>
+		</div>
+			
+		<div class="item">
+			<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1" style="padding-bottom: 45px;">
+				<h1 style="font-size: 3.5em; margin-top: 30px;">Sell!</h1>
+				<p style="font-size: 1.5em;">Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem Lorum ipsum dolem.</p>
+			</div>
+		</div>
+	</div>
+
+	<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
+	-->
 	<!-- Devices >= Small -->
 	<div class="row hidden-xs">
 		<div class="col-sm-7">
