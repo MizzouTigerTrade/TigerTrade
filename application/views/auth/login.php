d68b048333f221a288cbe112dfdc45d3f434b7e9
kylecarlson <krcz85@mail.missouri.edu>
2015-04-23 01:02:49 -0500

    css

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,4 +1,5 @@
 <div class="container padding-top-20">
+<div class="container-border">
 	<div class="row">
 		<div class="col-xs-12">
 			<h1>Login</h1>
@@ -64,4 +65,5 @@
             </div>
 
       <?php echo form_close();?>
+</div>
 </div>
