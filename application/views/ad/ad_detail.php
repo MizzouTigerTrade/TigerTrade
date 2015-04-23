999fbc5a2b375a5f9ad27e44cd12999b25e14478
kylecarlson <krcz85@mail.missouri.edu>
2015-04-23 00:57:19 -0500

    css

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -1,4 +1,5 @@
 <div class="container padding-top-20">
+<div class="container-border">
 	<div class="row">
 		<div class="col-xs-12">
 			<h1><?php echo $ad->title; ?></h1>
@@ -252,4 +253,5 @@
 			</div>
 		</div>
 	<?php } ?>
+</div>
 </div>
