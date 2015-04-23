5fb258ed5dd8c1e570274a2b7a215b8a4fe06e67
kylecarlson <krcz85@mail.missouri.edu>
2015-04-23 01:56:25 -0500

    css

--- application/controllers/content.php
+++ application/controllers/content.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Content extends CI_Controller {
+class Content extends Admin_Controller {
 
 	/**
 	 * Index Page for this controller.
---
+++ application/core/Admin_controller.php
@@ -0,0 +1,38 @@
+<?php
+class Admin_Controller extends CI_Controller
+{
+    protected $calledClass ;
+    protected $calledMethod;
+    protected $isAuthException;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->library("router");
+
+        /* 
+            add the controllers and the methods which don't need auth check.
+            This is to assign any controller and it's methods to skip the auth
+            check.
+
+            Format : "{CONTROLLER}" => "{A METHOD}", "{Another METHOD}",
+        */
+
+        $authExceptions = array(
+
+            "admin"     => array("login", "logout", "create_user")
+
+        );
+
+        $this->calledClass = $this->router->fetch_class();
+        $this->calledMethod = $this->router->fetch_method();
+
+        $this->isAuthException = array_key_exists($this->calledClass,$authExceptions) && in_array($this->calledMethod, $authExceptions[$this->calledClass]);
+
+        if(!$this->isAuthException && !isset($this->session->userdata('admin_id')))
+        {
+            redirect('auth/login.php');
+        }
+    }
+}
