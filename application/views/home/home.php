db3e407e70a34e97161a8e0e92896eb157f1221f
kylecarlson <krcz85@mail.missouri.edu>
2015-04-22 19:37:08 -0500

    css

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -14,7 +14,7 @@
 			Login
 			<?php echo form_open("auth/login", array('class' => 'form-horizontal', 'id' => 'ad-form'));?>
             <div class="form-group">
-                  <label for="identity" class="col-xs-12 control-label label-20">Email</label>
+                  <label for="identity" class="col-xs-12 control-label">Email</label>
                   <div class="col-xs-12">
                         <?php
 	                         $data = array(
@@ -27,7 +27,7 @@
                   </div>
             </div>
             <div class="form-group" style="margin-bottom: 0px;">
-                  <label for="password" class="col-sm-4 control-label label-20">Password</label>
+                  <label for="password" class="col-sm-4 control-label">Password</label>
                   <div class="col-sm-4">
 	                    <?php
 	                         $data = array(
