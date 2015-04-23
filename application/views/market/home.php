f3f78aa84817ea9e72c21b6538fbd298707ad3b3
timvanhorn <timvanhorn@ymail.com>
2015-04-22 19:33:00 -0500

    test

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -163,7 +163,7 @@ $(document).ready(function(){
 									}
 									if(empty($image_link)) { ?> 
 									<!--<img class="img-thumbnail" src="http://placehold.it/500x500" alt="" width="100%" height="100%">-->
-									<img class="img-thumbnail" src="http://thetigertrade.com/assets/Images/defaultImage.png" alt="" width="100%" height="100%">
+									<img class="img-thumbnail" src="http://thetigertrade.com/assets/Images/defaultImage.jpg" alt="" width="100%" height="100%">
 								<?php } else { ?>
 									<img class="img-thumbnail" src="<?php echo $image_link; ?>" alt="Error loading image" width="100%" height="100%">
 							<?php } ?>
