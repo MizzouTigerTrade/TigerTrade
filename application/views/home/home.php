70313e01819893eff061bce77df59f171e5e1229
kylecarlson <krcz85@mail.missouri.edu>
2015-04-22 19:35:36 -0500

    css

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -10,8 +10,56 @@
 	<hr>
 	
 	<div class="row">
-		<div class="col-sm-5 col-sm-offset-1">
+		<div class="col-xs-12">
 			Login
+			<?php echo form_open("auth/login", array('class' => 'form-horizontal', 'id' => 'ad-form'));?>
+            <div class="form-group">
+                  <label for="identity" class="col-sm-4 control-label label-20">Email</label>
+                  <div class="col-sm-4">
+                        <?php
+	                         $data = array(
+					          'name'        => 'identity',
+					          'id'          => 'identity',
+					          'class'       => 'form-control'
+					        );
+	                        echo form_input($data);
+	                    ?>
+                  </div>
+            </div>
+            <div class="form-group" style="margin-bottom: 0px;">
+                  <label for="password" class="col-sm-4 control-label label-20">Password</label>
+                  <div class="col-sm-4">
+	                    <?php
+	                         $data = array(
+					          'name'        => 'password',
+					          'id'          => 'password',
+					          'class'       => 'form-control'
+					        );
+	                        echo form_password($data);
+	                    ?>
+						<p class="help-block"><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+                  </div> 
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-4 col-sm-4">
+	                <div class="checkbox">
+					    <label style="padding-top: 7px;">
+                        <?php
+	                         $data = array(
+					          'name'        => 'remember',
+					          'id'          => 'remember',
+					          'class'       => ''
+					        );
+	                        echo form_checkbox($data);
+	                    ?>Remember me
+					    </label>
+					    <button type="submit" style="float: right;" class="btn btn-default">Login</button>
+	                </div>
+	                
+                </div>
+            </div>
+
+			<?php echo form_close();?>			
 		</div>
 		<div class="col-sm-5">
 			Register
