abd95240aff648122c2db2c780520406cb849c8f
kylecarlson <krcz85@mail.missouri.edu>
2015-04-23 01:29:27 -0500

    css

--- application/views/ad/edit_ad.php
+++ application/views/ad/edit_ad.php
@@ -40,14 +40,16 @@ $(document).ready(function (){
 </script>
 
 <div class="container padding-top-20">
+<div class="container-border">
 	<div class="row">
-		<div class="col-xs-12">
+		<div class="col-xs-10 col-xs-offset-1">
 			<h1>Edit Ad Form</h1>
 		</div>
 	</div>
-
+	
 	<hr>
-
+	
+	<div style="padding: 0 15px;">
 	<?php if(isset($created)) {?>
 	    <div class="alert alert-success">
 	        <a href="#" class="close" data-dismiss="alert">&times;</a>
@@ -205,5 +207,7 @@ $(document).ready(function (){
 			</div>
 		</div>
 	<?php echo form_close();?>
+	</div>
+</div>
 	
 </div>
