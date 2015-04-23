c8b887775ddbf26cf2ad8deba773a6c97a1ba84f
kylecarlson <krcz85@mail.missouri.edu>
2015-04-23 01:15:44 -0500

    css

--- application/views/forms/new_ad.php
+++ application/views/forms/new_ad.php
@@ -41,16 +41,14 @@ $(document).ready(function (){
 </script>
 
 <div class="container padding-top-20">
-<div class="container-border">
 	<div class="row">
-		<div class="col-xs-10 col-xs-offset-1">
+		<div class="col-xs-12">
 			<h1>New Ad Form</h1>
 		</div>
 	</div>
 
 	<hr>
-	
-	<div style="padding: 0 15px;" class="visible-xs">
+
 	<?php if(isset($created)) {?>
 	    <div class="alert alert-success">
 	        <a href="#" class="close" data-dismiss="alert">&times;</a>
@@ -83,7 +81,7 @@ $(document).ready(function (){
 		<div class="form-group" id="categoryForm">
 			<label for="category" class="col-sm-2 control-label label-20">Category</label>
 			<div class="col-sm-10">
-			<select name="category" class="form-control" id="categorySelectForm"> 
+			<select name="category" id="categorySelectForm"> 
 				<option value="">Select One</option>
 				<?php
 					foreach($categories->result() as $category)
@@ -159,7 +157,5 @@ $(document).ready(function (){
 			<img id="pic1" src="#" alt="your image" />
 		</div>
 	</div>
-	</div>
-</div>
 	
 </div>
