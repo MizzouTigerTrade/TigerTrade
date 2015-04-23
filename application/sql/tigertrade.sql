ec38275f00ea5628793087303a2cb89f5ffd2f12
timvanhorn <timvanhorn@ymail.com>
2015-04-22 22:40:33 -0500

    test

--- application/sql/tigertrade.sql
+++ application/sql/tigertrade.sql
@@ -47,8 +47,8 @@ CREATE TABLE flags (
 	ad_id INTEGER,
 	user_id INTEGER,
 	PRIMARY KEY(ad_id, user_id),
-	FOREIGN KEY(ad_id) REFERENCES ads(ad_id),
-	FOREIGN KEY(user_id) REFERENCES users(id)
+	FOREIGN KEY(ad_id) REFERENCES ads(ad_id) ON DELETE CASCADE,
+	FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- Table: kylecarlson_tigertrade.ads
