4e591acd6775ccb35ba89cf5cc13a684fb6f690c
timvanhorn <timvanhorn@ymail.com>
2015-04-25 17:10:21 -0500

    test

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -113,7 +113,7 @@
 								<div class="modal-footer">
 									<input class="btn btn-xs btn-primary" type="submit" value="Yes">
 									</form>
-									<button type="button" class="btn btn-xs btn-default" data-dismiss="modal">Cancel</button>
+									<button type="button" class="btn btn-xs btn-danger" data-dismiss="modal">Cancel</button>
 								</div>
 							</div>
 						</div>
