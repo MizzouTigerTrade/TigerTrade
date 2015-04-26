37cab8a04a2582fea2dfd16dad5225e89b7e5226
timvanhorn <timvanhorn@ymail.com>
2015-04-25 20:45:06 -0500

    update ad details

--- application/views/ad/edit_ad.php
+++ application/views/ad/edit_ad.php
@@ -43,7 +43,7 @@ $(document).ready(function (){
 <div class="container-border">
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1">
-			<h1>Edit Ad Form</h1>
+			<h1>Edit Ad</h1>
 		</div>
 	</div>
 	
