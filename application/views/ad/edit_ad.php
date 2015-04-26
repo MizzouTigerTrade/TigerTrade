901620a04a98901396fd8fe753e703f4321ccf65
timvanhorn <timvanhorn@ymail.com>
2015-04-25 19:26:37 -0500

    change button color

--- application/views/ad/edit_ad.php
+++ application/views/ad/edit_ad.php
@@ -203,7 +203,7 @@ $(document).ready(function (){
 		</div>
 		<div class="form-group">
 			<div class="col-sm-offset-2 col-sm-10">
-				<button type="submit" class="btn btn-default">Edit</button>
+				<button type="submit" class="btn btn-primary">Save</button>
 			</div>
 		</div>
 	<?php echo form_close();?>
