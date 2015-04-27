a31acd4774e91ba987e0b5bd653f36436bcc1024
kylecarlson <krcz85@mail.missouri.edu>
2015-04-27 02:40:28 -0500

    css

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -382,7 +382,7 @@
 				</div>
 				<div class="form-group">
 					<div class="col-xs-12">
-						<button type="submit" class="btn btn-primary">Comment</button>
+						<button type="submit" class="btn btn-primary text-right">Comment</button>
 					</div>
 				</div>
 			</div>
