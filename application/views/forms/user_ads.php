4a73cb083c0c3cde6f63862a79785b759bb8f0c1
timvanhorn <timvanhorn@ymail.com>
2015-04-26 21:46:26 -0500

    modal

--- application/views/forms/user_ads.php
+++ application/views/forms/user_ads.php
@@ -39,7 +39,7 @@
 			<td><a href="<?php echo base_url('ad/edit/' . $ad['ad_id'] ) ?>">Edit</a> | <a href="" data-toggle="modal" data-target="#deleteModal<?php echo $ad['ad_id']; ?>">Delete</a>
 	
 					<div class="modal fade" id="deleteModal<?php echo $ad['ad_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-						<div class="modal-dialog" style="width: 400px;">
+						<div class="modal-dialog" style="width: 425px;">
 							<div class="modal-content">
 								<div class="modal-header">
 									<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
