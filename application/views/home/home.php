2bdae7ff3505b0472e1a1a738f90061931f8c097
kylecarlson <krcz85@mail.missouri.edu>
2015-04-27 17:59:40 -0500

    css

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -21,58 +21,54 @@
 	</div>
 	-->
 	
+	
+	<?php if ($message != "") { ?>
 	<div style="padding: 0 15px;">
-      <?php if ($message != "") { ?>
-      <div id="infoMessage">
-		<div class="alert alert-info" role="alert" style="margin-top: 10px;">
-		  <span class="sr-only">Error:</span>
-		  <?php echo $message;?>
+		<div id="infoMessage">
+			<div class="alert alert-error" role="alert" style="margin-top: 10px;">
+			<span class="sr-only">Error:</span>
+			<?php echo $message;?>
 		</div>
-	  </div>
-	  <?php }; ?>
-      
-     <form action="<?php echo base_url('auth/login') ?>" method="POST" class="form-horizontal" id="ad-form" data-toggle="validator" role="form">
+	</div>
+	<?php }; ?>
+	
+	<form action="<?php echo base_url('auth/login') ?>" method="POST" class="form-horizontal" id="ad-form" data-toggle="validator" role="form">
+	
+	<div class="form-group">
+		<label for="first_name" class="col-sm-4 control-label label-20">Email</label>
+		<div class="col-sm-4">
+			<input type="text" name="identity" class="form-control" id="identity" placeholder="example@mail.missouri.edu" required>
+		</div>
+		<div class="help-block with-errors"></div>
+	</div>
+	
+	<div class="form-group">
+		<label for="first_name" class="col-sm-4 control-label label-20">Password</label>
+		<div class="col-sm-4">
+			<input type="password" name="password" class="form-control" id="password" placeholder="password" required>
+		</div>
+		<div class="help-block with-errors"></div>
+	</div>
+	
+	<div class="form-group">
+		<div class="col-sm-offset-4 col-sm-4">
+			<div class="checkbox">
+				<label style="padding-top: 7px;">
+					<?php
+					$data = array(
+					'name'        => 'remember',
+					'id'          => 'remember',
+					'class'       => ''
+					);
+					echo form_checkbox($data);
+					?>Remember me
+				</label>
+				<button type="submit" style="float: right;" class="btn btn-primary">Login</button>
+			</div>
+		</div>
+	</div>
+	
+	<?php echo form_close();?>
 
-      <div class="form-group">
-            <label for="first_name" class="col-sm-4 control-label label-20">Email</label>
-            <div class="col-sm-4">
-                  <input type="text" name="identity" class="form-control" id="identity" placeholder="example@mail.missouri.edu" required>
-            </div>
-            <div class="help-block with-errors"></div>
-      </div>
-      <div class="form-group">
-            <label for="first_name" class="col-sm-4 control-label label-20">Password</label>
-            <div class="col-sm-4">
-                  <input type="password" name="password" class="form-control" id="password" placeholder="password" required>
-            </div>
-            <div class="help-block with-errors"></div>
-      </div>
-
-      <div class="form-group">
-          <div class="col-sm-offset-4 col-sm-4">
-              <div class="checkbox">
-                <label style="padding-top: 7px;">
-                        <?php
-                             $data = array(
-                      'name'        => 'remember',
-                      'id'          => 'remember',
-                      'class'       => ''
-                    );
-                            echo form_checkbox($data);
-                        ?>Remember me
-                </label>
-                <button type="submit" style="float: right;" class="btn btn-primary">Login</button>
-              </div>
-              
-          </div>
-        </div>
-
-      <?php echo form_close();?>
-        </div>
-        <div class="form-group">
-          <div class="col-sm-offset-4 col-sm-4">
-            <a href="<?php echo base_url('auth/create_user') ?>" class="btn btn-primary">Register</a>
-          </div>
-        </div>
 </div>
 </div>
