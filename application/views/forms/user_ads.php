8417d37cbcdf1f10697049f8b2030b8072d94379
timvanhorn <timvanhorn@ymail.com>
2015-04-26 21:35:44 -0500

    test

--- application/views/forms/user_ads.php
+++ application/views/forms/user_ads.php
@@ -36,7 +36,7 @@
 			echo '<td>'.$ad['category'].'</td>';
 			echo '<td>'.$ad['subCategory'].'</td>'; ?>
 			
-			<td><a href="'.base_url('ad/edit/'.$ad['ad_id']).'">Edit</a> | <a data-toggle="modal" data-target="#deleteModal">Delete</a>
+			<td><a href="'.base_url('ad/edit/'.$ad['ad_id']).'">Edit</a> | <a href="" data-toggle="modal" data-target="#deleteModal">Delete</a>
 	
 					<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 						<div class="modal-dialog" style="width: 350px;">
