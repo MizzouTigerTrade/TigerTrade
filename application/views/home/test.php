3575e58fd7d11ef5f07eccdf1c1e15a7de3e261e
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-27 18:30:26 -0500

    php

--- application/views/home/test.php
+++ application/views/home/test.php
@@ -1,22 +1,28 @@
 <script src="<?php echo base_url('assets/js/bootstrap-table.min.js') ?>"></script>
-<table data-toggle="table">
+<script type="text/javascript">
+	
+	function queryParams() {
+    return {
+        type: 'owner',
+        sort: 'updated',
+        direction: 'desc',
+        per_page: 100,
+        page: 1
+    };
+}
+</script>
+<table data-toggle="table"
+       data-url="https://api.github.com/users/wenzhixin/repos"
+       data-query-params="queryParams"
+       data-pagination="true"
+       data-search="true"
+       data-height="300">
     <thead>
-        <tr>
-            <th>Item ID</th>
-            <th>Item Name</th>
-            <th>Item Price</th>
-        </tr>
+    <tr>
+        <th data-field="name">Name</th>
+        <th data-field="stargazers_count">Stars</th>
+        <th data-field="forks_count">Forks</th>
+        <th data-field="description">Description</th>
+    </tr>
     </thead>
-    <tbody>
-        <tr>
-            <td>1</td>
-            <td>Item 1</td>
-            <td>$1</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Item 2</td>
-            <td>$2</td>
-        </tr>
-    </tbody>
 </table>
