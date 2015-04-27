2590a538adb11f4c16cd5789becf0898b166d81d
kylecarlson <krcz85@mail.missouri.edu>
2015-04-27 01:37:23 -0500

    css

--- application/views/error/custom_404.php
+++ application/views/error/custom_404.php
@@ -2,14 +2,19 @@
 <div class="container-border">
 	
 	<div class="row">
-		<div class="col-xs-12">
+		<div class="col-xs-10 col-xs-offset-1">
 			<h1>404: Not Found</h1>
 		</div>
 	</div>
-
+	
 	<hr>
+	
+	<div class="row">
+		<div class="col-xs-10 col-xs-offset-1" style="padding-bottom: 10px;">
 
-	<p>We're sorry, but there's no content here!</p>
+			<p>We're sorry, but there's no content here!</p>
+		</div>
+	</div>
 	
 </div>
 </div>
