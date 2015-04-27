7a3b37bad65a4b728f365fca984225417ecdcdd4
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-27 18:31:11 -0500

    js

--- application/views/home/test.php
+++ application/views/home/test.php
@@ -1,3 +1,5 @@
+<script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
 <script src="<?php echo base_url('assets/js/bootstrap-table.min.js') ?>"></script>
 <script type="text/javascript">
 	
