7dd5fbedaf9a5429e24c14438e39971c53099b9c
timvanhorn <timvanhorn@ymail.com>
2015-04-26 20:54:42 -0500

    change button color

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -374,7 +374,7 @@
 				</div>
 				<div class="form-group">
 					<div class="col-sm-10" style="padding: 5px">
-						<button type="submit" class="btn btn-default">Submit</button>
+						<button type="submit" class="btn btn-primary">Submit</button>
 					</div>
 				</div>
 			</div>
