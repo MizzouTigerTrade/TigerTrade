d9323466c195fdcb6057a0559790cad2dd01c2ec
timvanhorn <timvanhorn@ymail.com>
2015-04-27 14:10:58 -0500

    test

--- application/views/team/index.php
+++ application/views/team/index.php
@@ -18,22 +18,22 @@
 
 	<div class="row" style="padding: 0 15px;">
 		<div class="col-xs-10 col-xs-offset-1"><h2>Kyle Carlson</h2></div>
-		<div class="col-sm-3 col-sm-offset-1"><img class="img-thumbnail" src="assets/Images/team/kyle.jpg" width="100%" height="100%"></div>
+		<div class="col-sm-3 col-sm-offset-1"><img class="img-thumbnail" src="" width="100%" height="100%"></div>
 		<div class="col-sm-7 team-member-description">DESCRIPTION</div>	
 	</div>
 	<div class="row" style="padding: 0 15px;">
 		<div class="col-xs-10 col-xs-offset-1"><h2>Jason McQuinn</h2></div>
-		<div class="col-sm-3 col-sm-offset-1"><img src="../../../assets/Images/team/jason.jpg" width="100%" height="100%"></div>
+		<div class="col-sm-3 col-sm-offset-1"><img class="img-thumbnail" src="" width="100%" height="100%"></div>
 		<div class="col-sm-7 team-member-description">DESCRIPTION</div>	
 	</div>
 	<div class="row" style="padding: 0 15px;">		
 		<div class="col-xs-10 col-xs-offset-1"><h2>Tim Gilman</h2></div>
-		<div class="col-sm-3 col-sm-offset-1"><img class="img-thumbnail" src="../../assets/Images/team/timg.jpg" width="100%" height="100%"></div>
+		<div class="col-sm-3 col-sm-offset-1"><img class="img-thumbnail" src="" width="100%" height="100%"></div>
 		<div class="col-sm-7 team-member-description">DESCRIPTION</div>	
 	</div>
 	<div class="row" style="padding: 0 15px;">		
 		<div class="col-xs-10 col-xs-offset-1"><h2>Tim Van Horn</h2></div>
-		<div class="col-sm-3 col-sm-offset-1"><img class="img-thumbnail" src="../../../assets/Images/team/timv.jpg" width="100%" height="100%"></div>
+		<div class="col-sm-3 col-sm-offset-1"><img class="img-thumbnail" src="" width="100%" height="100%"></div>
 		<div class="col-sm-7 team-member-description">DESCRIPTION</div>	
 	</div>
 </div>
