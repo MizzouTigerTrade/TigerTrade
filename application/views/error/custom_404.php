573ece8175956653a45feea65a4c6864daa391d4
kylecarlson <krcz85@mail.missouri.edu>
2015-04-27 01:34:48 -0500

    css

--- application/views/error/custom_404.php
+++ application/views/error/custom_404.php
@@ -1,4 +1,6 @@
 <div class="container padding-top-20">
+<div class="container-box">
+	
 	<div class="row">
 		<div class="col-xs-12">
 			<h1>404: Not Found</h1>
@@ -9,4 +11,5 @@
 
 	<p>We're sorry, but there's no content here!</p>
 	
+</div>
 </div>
