d7f1d5af91d361a78d3d1de1c17f9a9c6ed90f5a
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-27 16:54:20 -0500

    php

--- application/views/ad/ad_detail.php
+++ application/views/ad/ad_detail.php
@@ -388,7 +388,7 @@
 					<div class="panel-heading">
 						<div class="row">
 							<div class="col-xs-6">
-								<?php if ($this->ion_auth->user()->row()->id == $row->user_id) { echo 'Seller'; } else { echo 'Buyer'; } ?>
+								<?php if ($ad->user_id == $row->user_id) { echo 'Seller'; } else { echo 'Buyer'; } ?>
 							</div>
 							<div class="col-xs-6 text-right">
 								<?php echo $row->comment_time; ?>
