c88a77534c60e56f3c45a0cda312542cc1e56213
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-28 18:40:55 -0500

    html

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -143,61 +143,22 @@ function queryParams() {
 				<!-- Display Ads: rows of 1 -->
 				<br>
 				
-				<div class="row" style="padding-bottom: 15px;" id="emptySearch"></div>
-				<?php if(count($ads->result()) == 0)
-					{ ?>
-						<div class="row">No ads available.</div>
-				<?php } ?>
-				<?php foreach ($ads->result() as $row) { ?>
-				<a href="<?php echo base_url('/ad/details/' . $row->ad_id) ?>">
-					
-					<div class="row ad_display" style="padding-bottom: 15px;" id="<?= $row->ad_id ?>">
-						<div class="" style="margin-top: 20px; margin-bottom: 20px;">
-							
-							<div class="col-xs-3 col-md-2 col-md-offset-1">
-							<?php 	
-									$flag = 0;
-									foreach($images->result() as $img) { 
-										if($img->ad_id == $row->ad_id && $flag == 0)
-										{
-											$image_link = base_url('/'.$img->image_path);
-											$flag++;
-										}
-									}
-									
-									if($flag == 0) { ?> 
-									<img class="img-thumbnail" src="<?php echo base_url('/assets/Images/defaultImage.jpg')?>" alt="" width="100%" height="100%">
-								<?php } else { ?>
-									<img class="img-thumbnail" src="<?php echo $image_link; ?>" onerror="this.src='<?php echo base_url('/assets/Images/defaultImage.jpg')?>'" width="100%" height="100%">
-							<?php } ?>
-							</div>
-							
-							<div class="col-xs-9 col-md-8 search">
-								<h4 class="media-heading"><?php echo $row->title; ?>: $<?php echo $row->price; ?></h4>
-								<div class="row">
-									<div class="col-xs-12">
-								<?php
-								foreach($tags->result() as $tag) { 
-									if($tag->ad_id == $row->ad_id)
-									{
-										echo '<span class="label label-default">' . $tag->description . '</span> ';
-									}
-								}
-								?>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-xs-12" style="margin-top: 5px;">
-										<?php echo $row->description; ?>
-									</div>
-								</div>
-							</div>
-							
-						</div>
-					</div>
-					
-				</a>
-				<? } ?>
+				<table data-toggle="table"
+			       data-url="<?php echo base_url('json/getJson2');?>"
+			       data-query-params="queryParams"
+			       data-pagination="true"
+			       data-search="true"
+			       data-height="700">
+			    <thead>
+			    <tr>
+			        <th data-field="ad_id">Ad id</th>
+			        <th data-field="image" data-formatter="imageFormatter">Image</th>
+			        <th data-field="title">title</th>
+			        <th data-field="tags" data-formatter="tagFormatter">tags</th>
+			        <th data-field="description">Description</th>
+			    </tr>
+			    </thead>
+			</table>
 			</div>
 		</div>
 	</div>
