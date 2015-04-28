06cf2169fed12064ad31e95fce68e74f90e49ea2
jasonmcquinn <jdmnr9@mail.missouri.edu>
2015-04-28 18:53:50 -0500

    styling

--- application/views/auth/reset_password.php
+++ application/views/auth/reset_password.php
@@ -2,7 +2,7 @@
 <div class="container-border">
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1">
-			<h1>Forgot Password</h1> 
+			<h1>Reset Password</h1> 
 		</div>
 	</div>
 
@@ -30,22 +30,18 @@
 				<label for="new_password"><?php echo sprintf(lang('reset_password_new_password_label'), $min_password_length);?></label> <br />
 				<?php echo form_input($new_password);?>
 			</div>
-			<div class="help-block with-errors"></div>
-		
-
-		
+		</div>
+		<div class="form-group">
 			<div class="col-sm-4 col-sm-offset-4">
 				<?php echo lang('reset_password_new_password_confirm_label', 'new_password_confirm');?> <br />
 				<?php echo form_input($new_password_confirm);?>
 			</div>
-			<div class="help-block with-errors"></div>
-
+		</div>
 		<?php echo form_input($user_id);?>
-		
+		<div class="form-group">
 			<div class="col-sm-4 col-sm-offset-4">
 				<button type="submit" style="padding: 7px; float: right;" class="btn btn-primary">Reset Password</button>
 			</div>
-			<!--<div class="help-block with-errors"></div>-->
 		</div>
 
 	<?php echo form_close();?>
