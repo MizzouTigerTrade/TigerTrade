6350cefdccef643ae576c0fe92fbe6e02f7630c2
jasonmcquinn <jdmnr9@mail.missouri.edu>
2015-04-28 17:24:54 -0500

    fix errors

--- application/views/auth/forgot_password.php
+++ application/views/auth/forgot_password.php
@@ -32,9 +32,10 @@
                   <input type="text" name="identity" class="form-control" id="identity" placeholder="example@mail.missouri.edu" required>
             </div>
             <div class="help-block with-errors"></div>
+			<button type="submit" style="padding: 7px; float: right;" class="btn btn-primary">Send Email</button>
       </div>
 
-      <!-- <?php echo form_open("auth/forgot_password", array('class' => 'form-horizontal', 'id' => 'ad-form'));?> -->
+      <!-- <?php echo form_open("auth/forgot_password", array('class' => 'form-horizontal', 'id' => 'ad-form'));?>
             <div class="form-group">
                 <div class="col-sm-4 col-sm-offset-4">
 					<div class="input">
@@ -44,12 +45,11 @@
 					          'id'          => 'identity',
 					          'class'       => 'form-control'
 					        );
-	                        //echo form_input($data);
 	                    ?>
 						<button type="submit" style="padding: 7px; float: right;" class="btn btn-primary">Send Email</button>
                   </div>
             </div>
-		</div>
+		</div> -->
 
       <?php echo form_close();?>
 	</div>
