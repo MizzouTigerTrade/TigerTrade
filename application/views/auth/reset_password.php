628daa3a399c581d37915472e4482810633d342e
jasonmcquinn <jdmnr9@mail.missouri.edu>
2015-04-28 18:24:25 -0500

    test

--- application/views/auth/reset_password.php
+++ application/views/auth/reset_password.php
@@ -1,18 +1,44 @@
-<h1><?php echo lang('reset_password_heading');?></h1>
+<div class="container padding-top-20">
+<div class="container-border">
+	<div class="row">
+		<div class="col-xs-10 col-xs-offset-1">
+			<h1>Forgot Password</h1> 
+		</div>
+	</div>
 
-<div id="infoMessage"><?php echo $message;?></div>
+	<hr>
+<!-- <h1><?php echo lang('reset_password_heading');?></h1>
+<div id="infoMessage"><?php echo $message;?></div> -->
+
+	<div style="padding: 0 15px;">
+      <div class="row">
+			<div class="col-sm-offset-4 col-sm-8">
+			    <p>Enter the email address you used to sign up.</p>
+                <p>TigerTrade will send you an email about resetting your password.</p>
+			</div>
+      </div>
+      <?php if ($message != "") { ?>
+      <div id="infoMessage">
+		<div class="alert alert-info" role="alert" style="margin-top: 10px;">
+		  <span class="sr-only">Error:</span>
+		  <?php echo $message;?>
+		</div>
+	  </div>
+	  <?php }; ?>
 
 <?php echo form_open('auth/reset_password/' . $code);?>
+	<div class="form-group">
+		<div class="col-sm-4 col-sm-offset-4">
+			<label for="new_password"><?php echo sprintf(lang('reset_password_new_password_label'), $min_password_length);?></label> <br />
+			<?php echo form_input($new_password);?>
+		</div>
 
-	<p>
-		<label for="new_password"><?php echo sprintf(lang('reset_password_new_password_label'), $min_password_length);?></label> <br />
-		<?php echo form_input($new_password);?>
-	</p>
-
-	<p>
-		<?php echo lang('reset_password_new_password_confirm_label', 'new_password_confirm');?> <br />
-		<?php echo form_input($new_password_confirm);?>
-	</p>
+	<div class="form-group">
+		<div class="col-sm-4 col-sm-offset-4">
+			<?php echo lang('reset_password_new_password_confirm_label', 'new_password_confirm');?> <br />
+			<?php echo form_input($new_password_confirm);?>
+		</div>
+	</div>
 
 	<?php echo form_input($user_id);?>
 	
@@ -20,3 +46,5 @@
 	<p><?php echo form_submit('submit', lang('reset_password_submit_btn'));?></p>
 
 <?php echo form_close();?>
+</div>
+</div>
