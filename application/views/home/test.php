a7716e50d0298fe8d3f462008b6e283ef34385f4
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-27 22:20:24 -0500

    js

--- application/views/home/test.php
+++ application/views/home/test.php
@@ -4,33 +4,27 @@
 
 <script type="text/javascript">
 
-	function queryParams() {
-    return {
-        type: 'owner',
-        sort: 'updated',
-        direction: 'desc',
-        per_page: 100,
-        page: 1
-    };
+	function nameFormatter(value) {
+    return '<img class="img-thumbnail" src="http://thetigertrade.com/assets/Images/defaultImage.jpg" alt="" width="100%" height="100%">'
+}
 
-	 function imageFormatter(value) {
-		return '<img class="img-thumbnail" src="http://thetigertrade.com/assets/Images/defaultImage.jpg" alt="" width="100%" height="100%">';
-	};
+function starsFormatter(value) {
+    return '<i class="glyphicon glyphicon-star"></i> ' + value;
+}
 
+function forksFormatter(value) {
+    return '<i class="glyphicon glyphicon-cutlery"></i> ' + value;
+}
  
 }
 </script>
 <table data-toggle="table"
-       data-url="<?php echo base_url('json/getJson');?>"
-       data-query-params="queryParams"
-       data-pagination="true"
-       data-search="true"
-       data-height="700">
+       data-url="/gh/get/response.json/wenzhixin/bootstrap-table/tree/master/docs/data/data1/">
     <thead>
     <tr>
-        <th data-field="ad_id">Ad id</th>
-        <th data-field="image" data-formatter="imageFormatter">Image</th>
-        <th data-field="title">title</th>
+        <th data-field="name" data-formatter="nameFormatter">Name</th>
+        <th data-field="stargazers_count" data-formatter="starsFormatter">Stars</th>
+        <th data-field="forks_count" data-formatter="forksFormatter">Forks</th>
         <th data-field="description">Description</th>
     </tr>
     </thead>
