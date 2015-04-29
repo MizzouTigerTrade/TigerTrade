7babe78915707bb48eec374b731df67834cd8af9
timvanhorn <timvanhorn@ymail.com>
2015-04-28 20:03:14 -0500

    test

--- application/views/auth/deactivate_user.php
+++ application/views/auth/deactivate_user.php
@@ -13,12 +13,14 @@
 		<p><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></p>
 		
 		<?php echo form_open('auth/deactivate/' . $user->id, array('class' => 'form-horizontal'));?>
+		
+		
 
 			<div class="form-group">
-				<div class="radio">
+				<div class="radio col-sm-offset-3">
 				  <label><input type="radio" name="optradio" value="yes">Yes</label>
 				</div>
-				<div class="radio">
+				<div class="radio col-sm-offset-3">
 				  <label><input type="radio" name="optradio" value="no">No</label>
 				</div>
 			</div>
